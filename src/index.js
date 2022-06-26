36b36f4bcee3bbeb0b9c5b40f80a0ff58ef71dcc
MOCH EKO BUDI SETIAWAN <gseqoo@gmail.com>
2022-06-26 22:20:48 +0700

    add styling

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import ContactApp from './ContactApp';
 
-
+import './style.css';
 
 const root = createRoot(document.getElementById('root'));
 
